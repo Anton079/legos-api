c64f2521dddbe6ad92625af137176c4977d05570
HoriaAnton <horiaantonio@gmail.com>
2025-02-15 19:36:30 +0200

    first commit

---
+++ legos-api/Legos/Controllers/LegoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace legos_api.Legos.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class LegoController
+    {
+        private ILegoRepo _legoRepo;
+
+        public LegoController(ILegoRepo legoRepo)
+        {
+            _legoRepo = legoRepo;
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<List<Lego>>> GetLegoAsync()
+        {
+            var lego = await _legoRepo.GetLegosAsync();
+
+            return Ok(lego);
+        }
+    }
+}
